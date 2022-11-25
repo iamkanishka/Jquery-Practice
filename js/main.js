7ab0b571fa1327ade6454d150310685c3bfdc4bc
Kanishka.bc <kanishka.bc@molecularconnections.com>
2022-11-25 12:15:46 +0530

    16. jQuery After & Before

--- js/main.js
+++ js/main.js
@@ -1,23 +1,15 @@
 $(document).ready(function () {
-// $('#appendBtn').click(function(){
-//   $('#box').append("<h2>Hi this is kanishk Append</h2>")
-
-// });
-
-// $('#prependBtn').click(function(){
-//   $('#box').prepend("<h2>Hi this is kanishk Prepend</h2>")
-
-// })
-
-$('#appendBtn').click(function(){
-  $('ol').append("<li>List New</li>")
+$('#afterdBtn').click(function(){
+  $('#box').after("<h2>Hi this is kanishk After</h2>")
 
 });
 
-$('#prependBtn').click(function(){
-  $('ol').prepend("<li>List old</li>")
+$('#beforedBtn').click(function(){
+  $('#box').before("<h2>Hi this is kanishk Before</h2><p>This is a Paragraph</p>")
 
-});
+})
+
+
 
 
 });
