e416d178919865f86e944f3a6be2f0c96623f538
Kanishka.bc <kanishka.bc@molecularconnections.com>
2022-11-25 12:04:37 +0530

    15. jQuery Append & Prepend

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
-  $('#box').on({
-    click:function(){
-        $(this).css("background","orange");
-    },
-    mouseover:function(){
-      $(this).css("background","lightblue");
-      
-    },
-    mouseout:function(){
-      $(this).css("background","lightgreen");
-      
-    }
-  });
+// $('#appendBtn').click(function(){
+//   $('#box').append("<h2>Hi this is kanishk Append</h2>")
 
-  $("button").click(function(){
-    $('#box').off("mouseover mouseout")
-  })
+// });
+
+// $('#prependBtn').click(function(){
+//   $('#box').prepend("<h2>Hi this is kanishk Prepend</h2>")
+
+// })
+
+$('#appendBtn').click(function(){
+  $('ol').append("<li>List New</li>")
+
+});
+
+$('#prependBtn').click(function(){
+  $('ol').prepend("<li>List old</li>")
+
+});
 
 
 });
